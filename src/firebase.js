63e07bbb7daa171414e73fa7bd6bfa918108f3e0
Radheshyam kharol <73020015+radheshyamkharol121@users.noreply.github.com>
2025-08-25 22:41:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -19,5 +19,5 @@ export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const functions = getFunctions(app);
-
+export const analytics = getAnalytics(app);
 export default app;
