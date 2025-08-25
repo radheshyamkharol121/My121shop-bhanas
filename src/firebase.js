d7765d33ead563505bc96268f90853c3134e13ed
Radheshyam kharol <73020015+radheshyamkharol121@users.noreply.github.com>
2025-08-25 22:40:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getFunctions } from 'firebase/functions';
-
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyA3a6lSIE1952iZUkIfWtozzWlrZbcXM5E",
+  authDomain: "my121shop-56021.firebaseapp.com",
+  databaseURL: "https://my121shop-56021-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "my121shop-56021",
+  storageBucket: "my121shop-56021.firebasestorage.app",
+  messagingSenderId: "772439515291",
+  appId: "1:772439515291:web:6f1cd87e0117390726663e"
 };
 
 const app = initializeApp(firebaseConfig);
