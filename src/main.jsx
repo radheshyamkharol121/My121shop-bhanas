e8d76ad98214fe7500635c28049ed8afb305f502
Radheshyam kharol <73020015+radheshyamkharol121@users.noreply.github.com>
2025-08-25 22:13:36 +0530

    Update main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+// ऐप को रेंडर करें
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
